faeb0f50a84d9be16b4ae063d677ca1ec3c91ed1
sara kaouar <sara.kaouar.christol@gmail.com>
2023-04-24 12:58:56 +0200

    4 commit lire fichier json

--- app/src/main/java/fr/greta/fastfood/MainActivity.java
+++ app/src/main/java/fr/greta/fastfood/MainActivity.java
@@ -3,9 +3,11 @@ package fr.greta.fastfood;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.os.Handler;
+
+
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -18,35 +20,22 @@ import java.util.List;
 
 import fr.greta.fastfood.model.Restaurant;
 
-public class SplashActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//afficher limage pour 2000 s
-
-        ActionBar actionBar1 = getSupportActionBar();
-        actionBar1.hide();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, SplashActivity.class));
-                finish();
-            }
-        }, 2000);
-
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Restautrants list");
+       // ActionBar actionBar = getSupportActionBar();
+       // actionBar.setTitle("Restautrants list");
         //get the restaurant list from the json
         List<Restaurant> restaurantList = getRestaurantData();
         //injevtion first view
-        initRecyclerView(restaurantList);
+
 
     }
+
 
     private List<Restaurant> getRestaurantData() {
         //lis le json
